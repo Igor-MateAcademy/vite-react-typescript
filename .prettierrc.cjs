a104bd4e218b0337bef305775109b0611750c433
Igor Kovalev <bizzzy0@gmail.com>
2022-11-03 12:03:43 +0200

    Initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  "singleQuote": true,
+    "trailingComma": "es5",
+    "bracketSpacing": true,
+    "tabWidth": 2,
+    "proseWrap": "preserve",
+    "arrowParens": "avoid",
+    "semi": true,
+    "printWidth": 120,
+    "endOfLine": "auto",
+    "prettier.cssEnable": [
+      "css",
+      "less",
+      "scss"
+    ]
+};
---
+++ src/containers/App/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// styles
+import 'sources/styles/styles.scss';
+import 'antd/dist/antd.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App" style={{ background: 'white' }}>
+      My app
+    </div>
+  );
+};
+
+export default App;
